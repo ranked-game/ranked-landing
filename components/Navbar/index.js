ab45047905bdec552c07e54414d6e08023599784
Iryna Sirenko <maskyss932@gmail.com>
2019-08-21 17:12:57 +0300

    styling navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,9 +1,20 @@
 import Styles from './styles.module.scss';
 // import { BlockWithPhone } from '../BlockWithPhone';
+import Logo from '../../static/assets/images/logoGroup.png';
+// import Logo from '../../static/assets/logoGroup.svg';
 
 const Navbar = () => (
 	<div className={Styles.container}>
-		<p>//</p>
+		<img className={Styles.logo} src={Logo} alt={'logo'} />
+		<div className={Styles.containerBtnUrl}>
+			<button className={Styles.btnUrl}>Games avaible</button>
+			<button className={Styles.btnUrl}>Features</button>
+			<button className={Styles.btnUrl}>What you will get</button>
+		</div>
+		<div className={Styles.containerBtnDwnl}>
+			<button className={Styles.btnUrl}>Sign In/Up</button>
+			<button className={Styles.btnDwnl}>Download</button>
+		</div>
 	</div>
 );
 
