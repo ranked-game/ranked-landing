ecfd67b2428be47f1b39993eeaed407cc2eaa25b
roquefore <roquefore@gmail.com>
2019-08-21 15:24:22 +0300

    moved assets to /static

--- components/StartupPage/index.js
+++ components/StartupPage/index.js
@@ -1,19 +1,10 @@
 import Styles from './styles.module.scss';
-// import { BlockWithPhone } from '../BlockWithPhone';
-
-//Instruments
-import img from '../../theme/svg/logo.png';
 
 const StartupPage = () => (
-	<div
-		className={Styles.container}
-		style={{
-			backgroundImage: img,
-		}}
-	>
-		<div className={Styles.topLinear} />
-		<div className={Styles.bottomLinear} />
-	</div>
+    <div className={Styles.container}>
+        <div className={Styles.topLinear} />
+        <div className={Styles.bottomLinear} />
+    </div>
 );
 
 export default StartupPage;
