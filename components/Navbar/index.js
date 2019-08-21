7a08772cfca5f1adf0199231c124887f842ae97d
Iryna Sirenko <maskyss932@gmail.com>
2019-08-21 14:22:23 +0300

    startup structure

---
+++ components/Navbar/index.js
@@ -0,0 +1,10 @@
+import Styles from './styles.module.scss';
+// import { BlockWithPhone } from '../BlockWithPhone';
+
+const Navbar = () => (
+	<div className={Styles.container}>
+		<p>//</p>
+	</div>
+);
+
+export default Navbar;
---
+++ components/StartupPage/index.js
@@ -0,0 +1,10 @@
+import Styles from './styles.module.scss';
+// import { BlockWithPhone } from '../BlockWithPhone';
+
+const StartupPage = () => (
+	<div className={Styles.container}>
+		<p>//</p>
+	</div>
+);
+
+export default StartupPage;
