0cc94437db1e6e7bcd2a44dbad409d2ec1d25a73
Iryna Sirenko <maskyss932@gmail.com>
2019-08-21 15:05:58 +0300

    dont work background-image

--- components/StartupPage/index.js
+++ components/StartupPage/index.js
@@ -1,9 +1,18 @@
 import Styles from './styles.module.scss';
 // import { BlockWithPhone } from '../BlockWithPhone';
 
+//Instruments
+import img from '../../theme/svg/logo.png';
+
 const StartupPage = () => (
-	<div className={Styles.container}>
-		<p>//</p>
+	<div
+		className={Styles.container}
+		style={{
+			backgroundImage: img,
+		}}
+	>
+		<div className={Styles.topLinear} />
+		<div className={Styles.bottomLinear} />
 	</div>
 );
 
