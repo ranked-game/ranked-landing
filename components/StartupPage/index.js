dfec3f616fb54270c562c1e682305959790fc541
Iryna Sirenko <maskyss932@gmail.com>
2019-08-21 17:41:26 +0300

    trouble in startupPage/index.js

--- components/StartupPage/index.js
+++ components/StartupPage/index.js
@@ -1,10 +1,17 @@
 import Styles from './styles.module.scss';
-
+import Dashboard from '../../static/assets/images/dashboards.png';
+import Logo from '../../static/assets/images/logoGroup.png';
 const StartupPage = () => (
-    <div className={Styles.container}>
-        <div className={Styles.topLinear} />
-        <div className={Styles.bottomLinear} />
-    </div>
+	<div className={Styles.container}>
+		<div className={Styles.topLinear} />
+		<div className={Styles.bottomLinear} />
+		<div className={Styles.infoBlock}>
+			<div />
+		</div>
+		<img className={Styles.logo} src={Logo} alt={'logo'} />
+
+		<img className={Styles.dashboardImg} src={Dashboard} alt={'dashboard'} />
+	</div>
 );
 
 export default StartupPage;
