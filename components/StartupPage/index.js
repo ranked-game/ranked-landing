31fc9a89a8d20e0e44f1bfea969999652fde112d
Iryna Sirenko <maskyss932@gmail.com>
2019-08-29 12:58:19 +0300

    v1.0.0_TODO:animations and updated sizes SVG

--- components/StartupPage/index.js
+++ components/StartupPage/index.js
@@ -14,60 +14,60 @@ const Dashboard = '/static/assets/images/dashboard.png';
 const wave = '/static/assets/images/bottom2.png';
 
 class StartupPage extends Component {
-    state = {
-        secondsToStart: null,
-    };
+	state = {
+		secondsToStart: null,
+	};
 
-    componentDidMount = () => {
-        const db = Firebase.firestore();
+	componentDidMount = () => {
+		const db = Firebase.firestore();
 
-        db.collection('closedBetaStartDate')
-            .get()
-            .then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    const { date } = doc.data();
+		db.collection('closedBetaStartDate')
+			.get()
+			.then((querySnapshot) => {
+				querySnapshot.forEach((doc) => {
+					const { date } = doc.data();
 
-                    const goalTime = moment(date).utc();
-                    const now = moment().utc();
+					const goalTime = moment(date).utc();
+					const now = moment().utc();
 
-                    const diff = goalTime.diff(now, 'seconds');
+					const diff = goalTime.diff(now, 'seconds');
 
-                    this.setState({
-                        secondsToStart: diff,
-                    });
-                });
-            });
-    };
-    render() {
-        const { secondsToStart } = this.state;
+					this.setState({
+						secondsToStart: diff,
+					});
+				});
+			});
+	};
+	render() {
+		const { secondsToStart } = this.state;
 
-        return (
-            <div className={Styles.container}>
-                <div className={Styles.table}>
-                    <div className={Styles.infoBlock}>
-                        <div className={Styles.bigText}>lead the competition and win prizes</div>
-                        <div className={Styles.p1}>
-                            Our application is in beta testing, become one of the first players and
-                            get prizes.
-                        </div>
-                        <div className={Styles.p2}>Time to open beta:</div>
-                        {secondsToStart ? (
-                            <Timer seconds={secondsToStart} />
-                        ) : (
-                            <Spinner size="5rem" loader />
-                        )}
-                        <div className={Styles.btns}>
-                            <button className={Styles.fireBtnViolet}>Try beta</button>
-                            <button className={Styles.fireBtnOrange}>Download</button>
-                        </div>
-                    </div>
+		return (
+			<div className={Styles.container}>
+				<div className={Styles.table}>
+					<div className={Styles.infoBlock}>
+						<div className={Styles.bigText}>lead the competition and win prizes</div>
+						<div className={Styles.p1}>
+							Our application is in beta testing, become one of the first players and
+							get prizes.
+						</div>
+						<div className={Styles.p2}>Time to open beta:</div>
+						{secondsToStart ? (
+							<Timer seconds={secondsToStart} />
+						) : (
+							<Spinner size="5rem" loader />
+						)}
+						<div className={Styles.btns}>
+							<button className={Styles.fireBtnViolet}>Try beta</button>
+							<button className={Styles.fireBtnOrange}>Download</button>
+						</div>
+					</div>
 
-                    <img className={Styles.dashboardImg} src={Dashboard} alt={'dashboard'} />
-                </div>
-                <img className={Styles.wave} src={wave} alt={'wave'} />
-            </div>
-        );
-    }
+					<img className={Styles.dashboardImg} src={Dashboard} alt={'dashboard'} />
+				</div>
+				<img className={Styles.wave} src={wave} alt={'wave'} />
+			</div>
+		);
+	}
 }
 
 export default StartupPage;
