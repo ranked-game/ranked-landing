f3ad98688bb6c1f6c7fe399d6dba8e64d097e4b2
roquefore <roquefore@gmail.com>
2019-09-01 11:46:14 +0300

    [UPD] Navbar buttons/anchors + StartupPage buttons + Benefits

--- components/Benefit/index.js
+++ components/Benefit/index.js
@@ -3,21 +3,16 @@ import Styles from './styles.module.scss';
 
 const dotterLine = '../../static/assets/svg/dotterLine.svg';
 
-const Benefit = (props) => (
-	<div className={Styles.container}>
-		{props.position === 'right' ? (
-			<div className={Styles.right}>
-				<img className={Styles.imgBenefit} src={props.imgUrl} alt={'img'} />
-				<div className={Styles.text}>{props.text}</div>
-			</div>
-		) : (
-			<div className={Styles.left}>
-				<div className={Styles.text}>{props.text}</div>
-				<img className={Styles.imgBenefit} src={props.imgUrl} alt={'img'} />
-			</div>
-		)}
-		{props.dots && <img className={Styles.dotterLine} src={dotterLine} alt={'img'} />}
-	</div>
-);
+const Benefit = (props) => {
+    const { imgUrl, text, position, dots } = props;
+
+    return (
+        <div className={Styles.container}>
+            <div className={Styles[position]}>{text}</div>
+            <img className={Styles.imgBenefit} src={imgUrl} alt={'img'} />
+            {dots && <img className={Styles.dotterLine} src={dotterLine} alt={'img'} />}
+        </div>
+    );
+};
 
 export default Benefit;
