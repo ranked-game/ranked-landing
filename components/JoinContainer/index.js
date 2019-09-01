f4308220d5532af8a63b3892aec87f991b2f2964
roquefore <roquefore@gmail.com>
2019-09-01 11:58:34 +0300

    [UPD] Footer links + JoinContainer button

--- components/JoinContainer/index.js
+++ components/JoinContainer/index.js
@@ -4,18 +4,18 @@ import Styles from './styles.module.scss';
 import Fade from 'react-reveal/Fade';
 
 const JoinContainer = () => (
-	<div className={Styles.container}>
-		<Fade bottom>
-			<div className={Styles.bigText}>Want ot join closed beta?</div>
-			<div className={Styles.paragraph}>
-				Join closed beta testing now, help us in developing somthing cool and get cool
-				bonuses
-			</div>
-			<div className={Styles.buttons}>
-				<button className={Styles.buttonViolet}>Try beta</button>
-				<button className={Styles.buttonOrange}>Download</button>
-			</div>
-		</Fade>
-	</div>
+    <div className={Styles.container}>
+        <Fade bottom>
+            <div className={Styles.bigText}>Want ot join closed beta?</div>
+            <div className={Styles.paragraph}>
+                Join closed beta testing now, help us in developing somthing cool and get cool
+                bonuses
+            </div>
+            <div className={Styles.buttons}>
+                {/* <button className={Styles.buttonViolet}>Try beta</button> */}
+                <button className={Styles.buttonOrange}>Download</button>
+            </div>
+        </Fade>
+    </div>
 );
 export default JoinContainer;
