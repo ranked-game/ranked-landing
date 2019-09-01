4b6b28bfbf44582e888bfdce59e24fcb09ba5fcc
roquefore <roquefore@gmail.com>
2019-09-01 10:33:39 +0300

    service txts

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
 const withPlugins = require('next-compose-plugins');
 const withSass = require('@zeit/next-sass');
-// const withImages = require('next-images');
-module.exports = withPlugins([
-	[
-		{
-			webpack: (config) => {
-				config.module.rules.push({
-					test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-					use: {
-						loader: 'url-loader',
-						options: {
-							limit: 100000,
-						},
-					},
-				});
-				config.module.rules.push({
-					test: /\.svg$/,
-					use: 'file-loader',
-				});
-				// Fixes npm packages that depend on `fs` module
-				config.node = {
-					fs: 'empty',
-				};
 
-				return config;
-			},
-		},
-	],
-	[
-		withSass,
-		{
-			cssModules: true,
-		},
-	],
+module.exports = withPlugins([
+    [
+        {
+            webpack: (config) => {
+                config.module.rules.push({
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    use: {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 100000,
+                        },
+                    },
+                });
+                config.module.rules.push({
+                    test: /\.svg$/,
+                    use: 'file-loader',
+                });
+                // Fixes npm packages that depend on `fs` module
+                config.node = {
+                    fs: 'empty',
+                };
+
+                return config;
+            },
+        },
+    ],
+    [
+        withSass,
+        {
+            cssModules: true,
+        },
+    ],
 ]);
